3328afe301ad9c80cfb8061083fea43ebcf4a6a2
zoniha <awaawapurin@gmail.com>
2022-11-04 21:00:36 +0900

    layout: リセット用のCSSを追加

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createGlobalStyle } from 'styled-components';
 import { App } from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
@@ -7,3 +8,12 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <App />
   </React.StrictMode>
 );
+
+// Reset CSS
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
